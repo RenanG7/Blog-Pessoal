de4311e6ab5e5661c94b5bfcbdc4ccdc75d3f8ca
unknown <renaang7@gmail.com>
2023-04-17 10:54:41 -0300

    Conta model, Repository, Controller

--- BlogPessoal/src/main/java/com/generetion/blogpessoal/repository/PostagemRepository.java
+++ BlogPessoal/src/main/java/com/generetion/blogpessoal/repository/PostagemRepository.java
@@ -1,6 +1,10 @@
 package com.generetion.blogpessoal.repository;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generetion.blogpessoal.model.Postagem;
@@ -8,5 +12,6 @@ import com.generetion.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
-	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
 }
